37ba1918d146dc59bb01506a361be6413b0b4232
wellafbrni <104251599+wellafbrni@users.noreply.github.com>
2026-02-20 00:31:13 +0700

    Enhance code structure and clarity in main.py
    Refactor code for improved readability and maintainability, including cleaning up comments and organizing imports.

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
 import matplotlib
-matplotlib.use('Agg')  # Set matplotlib backend to 'Agg'
+matplotlib.use('Agg')  
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
@@ -40,17 +40,12 @@ from imblearn.over_sampling import SMOTE
 from sklearn.utils.class_weight import compute_class_weight
 from sklearn.metrics import f1_score, recall_score, precision_score, confusion_matrix, accuracy_score, classification_report
 
-# NAVBAR
-#['Home','Dataset', 'Preprocessing', 'Predict']
 st.set_page_config(layout='wide',initial_sidebar_state='collapsed',)
 
-# Inisialisasi session state
 if 'page' not in st.session_state:
     st.session_state.page = 'Home'
 
-# Spesifikasi menu utama
 menu_data = [
-    # {'icon': "far fa-copy", 'label': "Scrapping"},
     {'id': 'Data', 'icon': "🐙", 'label': "Dataset"},
     {'icon': "fas fa-tachometer-alt", 'label': "Preprocessing", 'ttip': "Ini adalah tooltip Dashboard!"},
     {'icon': "far fa-chart-bar", 'label': "Predict"},
@@ -62,13 +57,10 @@ menu_id = hc.nav_bar(
     menu_definition=menu_data,
     override_theme=over_theme,
     home_name='Home',
-    #login_name='Logout',
-    hide_streamlit_markers=False, #will show the st hamburger as well as the navbar now!
-    sticky_nav=True, #at the top or not
-    sticky_mode='pinned', #jumpy or not-jumpy, but sticky or pinned
+    hide_streamlit_markers=False, 
+    sticky_nav=True, 
+    sticky_mode='pinned', 
 )
-
-# Atur session state.page berdasarkan menu yang dipilih
 if menu_id == 'Home':
     st.session_state.page = 'Home'
 elif menu_id == 'Scrapping':
@@ -80,31 +72,21 @@ elif menu_id == 'Preprocessing':
 elif menu_id == 'Predict':
     st.session_state.page = 'Predict'
 
-# Mendapatkan id dari item menu yang diklik
-# st.info(f"Menu Terpilih: {menu_id}")
-
 def casefolding(text):
     text = text.lower()
     text = text.strip()
     return text
 
 def cleaning(text):
-    # Menghapus username (@username)
     text = re.sub(r'@\w+', '', text)
-    # Menghapus hashtag (#hashtag)
     text = re.sub(r'#\w+', '', text)
     text = re.sub(r'#\S+', '', text)
-    # Menghapus URL (http:// atau www.)
-    text = re.sub(r'http[s]?://\S+', '', text)  # Menghapus http atau https URL
-    text = re.sub(r'www\.\S+', '', text)        # Menghapus www URL
-    # Menghapus karakter yang bukan alfanumerik dan spasi
+    text = re.sub(r'http[s]?://\S+', '', text)  
+    text = re.sub(r'www\.\S+', '', text)       
     text = re.sub(r'[^0-9A-Za-z ]', '', text)
     text = re.sub(r'[^0-9A-Za-z\s]', ' ', text)
-    # Menghapus angka
     text = re.sub(r'\d+', '', text)
-    # Mengganti dua atau lebih spasi dengan satu spasi
     text = re.sub(r'\s+', ' ', text)
-    # Menghapus spasi di awal dan akhir teks
     text = text.strip()
     return text
 
@@ -113,16 +95,13 @@ indo_abbreviation = pd.read_csv('kamus_singkatan.csv', delimiter=';', names=col_
 indo_abbreviation.head()
 
 def replace_abbreviations(text, abbreviation):
-    # Buat dictionary dari dataframe indo_abbreviation
     abbreviation_dict = dict(zip(abbreviation['before'], abbreviation['after']))
 
-    # Fungsi untuk mengganti kata dalam text
     def replace_words(text):
         words = text.split()
         new_words = [abbreviation_dict[word] if word in abbreviation_dict else word for word in words]
         return ' '.join(new_words)
 
-    # Terapkan fungsi replace_words ke text
     text = replace_words(text)
 
     return text
@@ -132,16 +111,13 @@ indo_slank_words = pd.read_csv('kamusalay.csv', delimiter=',', names=col_names_s
 indo_slank_words.head()
 
 def replace_slank_words(text, slank_words):
-    # Buat dictionary dari dataframe indo_abbreviation
     abbreviation_dict = dict(zip(slank_words['before'], slank_words['after']))
 
-    # Fungsi untuk mengganti kata dalam text
     def replace_words(text):
         words = text.split()
         new_words = [abbreviation_dict[word] if word in abbreviation_dict else word for word in words]
         return ' '.join(new_words)
 
-    # Terapkan fungsi replace_words ke setiap text
     text = replace_words(text)
     return text
 
@@ -156,7 +132,6 @@ def remove_stop_words(text, preserved_words=None):
     clean_words = []
     text = text.split()
     for word in text:
-        # Periksa apakah kata ada dalam daftar kata yang ingin dipertahankan
         if preserved_words and word in preserved_words:
             clean_words.append(word)
         elif word not in stopwords_ind:
@@ -178,11 +153,9 @@ def tokenize(text):
     return text
 
 def remove_whitespace_and_combine(text, tokens):
-    if tokens and isinstance(tokens, list):  # Periksa apakah tokens bukan None dan merupakan list
-        # Menghapus whitespace yang tidak perlu
+    if tokens and isinstance(tokens, list):  
         tokens = [token.strip() for token in tokens if token and isinstance(token, str) and token.strip()]
 
-        # Menggabungkan kembali kata-kata menjadi kalimat yang padu
         result = ' '.join(tokens)
         return result
     else:
@@ -190,41 +163,37 @@ def remove_whitespace_and_combine(text, tokens):
 
 def preprocess_text(text):
     if isinstance(text, list):
-        # Jika input berupa daftar token, gabungkan menjadi string
         text = ' '.join(text)
 
-    if text is not None:  # Tambahkan pengecekan untuk nilai None
+    if text is not None:  
         text = casefolding(text)
         text = cleaning(text)
-        text = replace_abbreviations(text, indo_abbreviation)  # Menyertakan argumen 'abbreviation'
-        text = replace_slank_words(text, indo_slank_words)    # Menyertakan argumen 'slank_words'
+        text = replace_abbreviations(text, indo_abbreviation)  
+        text = replace_slank_words(text, indo_slank_words)    
         text = remove_stop_words(text)
         text = stemming(text)
-        text = ' '.join(tokenize(text))  # Ubah hasil tokenisasi menjadi string
+        text = ' '.join(tokenize(text))  
         
         return text
     else:
         return None
 
-# Fungsi untuk menyimpan dataset yang telah dilabeli
 def save_labeled_dataset(dataset, dataset_name):
     dataset_path = f"uploaded_files/labeled_{dataset_name}"
     dataset.to_csv(dataset_path, index=False)
     return dataset_path     
 
-# Fungsi untuk memuat dataset yang telah dilabeli
 def load_labeled_dataset(dataset_path):
     return pd.read_csv(dataset_path)
 
-# Variabel global untuk menyimpan dataset yang telah dilabeli
-labeled_datasets = {}  # Gunakan dictionary untuk menyimpan multiple datasets
+
+labeled_datasets = {}
 
 upload_dir = "uploaded_files"
 
 if not os.path.exists(upload_dir):
     os.makedirs(upload_dir)
 
-# HALAMAN SCRAPPING DATA TWITTER
 def get_tweet_data(card):
     """Extract data from tweet card"""
     try:
@@ -265,9 +234,8 @@ def get_tweet_data(card):
     tweet = (username, handle, postdate, text, reply_cnt, retweet_cnt, like_cnt)
     return tweet
 
-# Fungsi utama untuk scrapping
 def start_scraping(Acc_Username, Acc_Password, Tweets_Query, dataset_name):
-    driver = webdriver.Edge() # Gunakan driver yang sesuai untuk browser Anda
+    driver = webdriver.Edge()
     driver.get('https://twitter.com/login')
 
     username_input = WebDriverWait(driver, timeout=60).until(
@@ -277,7 +245,6 @@ def start_scraping(Acc_Username, Acc_Password, Tweets_Query, dataset_name):
     next_btn = driver.find_element(By.XPATH, '//*[text()="Next"]')
     next_btn.click()
 
-    # Tunggu untuk input password muncul
     password_input = WebDriverWait(driver, timeout=60).until(
         EC.visibility_of_element_located((By.XPATH, '//input[@name="password"]'))
     )
@@ -288,20 +255,17 @@ def start_scraping(Acc_Username, Acc_Password, Tweets_Query, dataset_name):
             
     time.sleep(5)
 
-    # Tunggu hingga tombol explore muncul dan klik
     explore_btn = WebDriverWait(driver, 60).until(
         EC.presence_of_element_located((By.XPATH, '//a[@href="/explore"]'))
     )
     explore_btn.click()
 
-    # Tunggu hingga input eksplorasi muncul
     explore_input = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((By.XPATH, '//input'))
     )
     explore_input.send_keys(Tweets_Query)
     explore_input.send_keys(Keys.ENTER)
 
-    # Tunggu hingga tombol latest muncul dan klik
     latest_btn = WebDriverWait(driver, 60).until(
         EC.presence_of_element_located((By.XPATH, '//div[@data-testid][2]/div/div[2]'))
     )
@@ -325,29 +289,25 @@ def start_scraping(Acc_Username, Acc_Password, Tweets_Query, dataset_name):
                         
         scroll_attempt = 0
         while True:
-            # cek posisi scroll
             driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
             time.sleep(3)
             curr_position = driver.execute_script("return window.pageYOffset;")
             if last_position == curr_position:
                 scroll_attempt += 1
                             
-                # akhir dari daerah scroll
                 if scroll_attempt >= 3:
                     scrolling = False
                     break
                 else:
-                    time.sleep(2) # Coba gulir lagi
+                    time.sleep(2) 
             else:
                 last_position = curr_position
                 break
 
-    # Pastikan folder "Assets" ada
     folder_name = 'Assets'
     if not os.path.exists(folder_name):
         os.makedirs(folder_name)
 
-    # Simpan dataset dengan nama yang ditentukan pengguna
     if dataset_name:
         file_name = f"{dataset_name}.csv"
         file_path = os.path.join(folder_name, file_name)
@@ -361,24 +321,19 @@ def start_scraping(Acc_Username, Acc_Password, Tweets_Query, dataset_name):
     st.success(f"Scraping data Twitter selesai. Data disimpan di dalam folder 'Assets' dengan nama '{file_name}'.")
     driver.quit()
 
-# Fungsi untuk menampilkan halaman sesuai session state
 if st.session_state.page == 'Home':
     
-    # Konten di halaman Home
     st.markdown("""
     <h3 style='text-align: center;'>ANALISIS SENTIMEN DALAM MENGETAHUI PERSEPSI PUBLIK TERHADAP REPRESENTASI KASUS LGBT DI MEDIA SOSIAL TWITTER MENGGUNAKAN PENDEKATAN NATURAL LANGUAGE PROCESSING DAN ALGORITMA NAIVE BAYES CLASSIFIER</h3>
     <h5 style='text-align: center;'></h5>
     """, unsafe_allow_html=True)
 
-   # Daftar gambar
     images = ['image1.webp','image2.png', 'image3.png']
 
-    # Tampilkan gambar secara berdampingan
     cols = st.columns(len(images))
 
     for col, img in zip(cols, images):
         with col:
-            # Menggunakan st.image untuk menampilkan gambar dan memastikan teks caption ditampilkan dengan benar
             col.image(img, width=350, use_column_width=False, caption=f' {img}')
     
     st.markdown("""
@@ -399,20 +354,13 @@ if st.session_state.page == 'Home':
 
     5. Tampilkan Hasil Pengujian: Lihat hasil analisis sentimen Anda di menu Klasifikasi Naive Bayes. Di sini, Anda akan menemukan metrik akurasi, precision, dan recall, serta confusion matrix untuk evaluasi yang lebih mendalam.""")     
 
-# HALAMAN DATASET
 elif st.session_state.page == 'Dataset':
     st.title('Silakan upload dataset terlebih dahulu')
     data_file = st.file_uploader("Unggah file CSV", type=["csv"])
 
-
     if data_file is not None:
-        # Membaca DataFrame dari file CSV yang diunggah
         df = pd.read_csv(data_file)
-        
-        # Menyimpan DataFrame ke dalam session state
         st.session_state.uploaded_data = df
-
-    # Memeriksa apakah ada data yang diunggah di session state
     if 'uploaded_data' in st.session_state:
         st.title('Data yang Telah Diunggah')
         df = st.session_state.uploaded_data
@@ -424,23 +372,18 @@ elif st.session_state.page == 'Dataset':
     <h1 style='text-align: center;'></h1>
     """, unsafe_allow_html=True)
 
-    #PART SCRAPPING DATA
     st.title('Scrapping Data Twitter')  
-    # Input untuk akun Twitter, topik pencarian, dan nama dataset
     Acc_Username = st.text_input("Masukkan username Twitter Anda:")
     Acc_Password = st.text_input("Masukkan password Twitter Anda:", type="password")
     Tweets_Query = st.text_input("Masukkan topik pencarian Twitter yang ingin di Scrapping:")
     dataset_name = st.text_input("Masukkan nama untuk dataset:")
 
-    # Tombol untuk memulai scraping
     if st.button("Mulai Scraping"):
-        # Periksa apakah akun Twitter, topik pencarian, dan nama dataset sudah dimasukkan
         if not Acc_Username or not Acc_Password or not Tweets_Query or not dataset_name:
             st.warning("Silakan masukkan username Twitter, password, topik pencarian, dan nama dataset terlebih dahulu.")
         else:
             start_scraping(Acc_Username, Acc_Password, Tweets_Query, dataset_name)
-
-#HALAMAN PREPROCESSING DATA        
+       
 elif st.session_state.page == 'Preprocessing':
     st.title('Text Preprocessing')
     if 'uploaded_data' in st.session_state:
@@ -463,12 +406,11 @@ elif st.session_state.page == 'Preprocessing':
             df['Tokenize_Text'] = df['Stemming_Text'].fillna('').apply(lambda x: tokenize(x))
             df['Processed_Text'] = df['Text'].fillna('').apply(lambda x: preprocess_text(x))
         
-        st.session_state.preprocessed_data = df  # Simpan dataframe hasil preprocessing
+        st.session_state.preprocessed_data = df
         st.dataframe(df[['Text','Case_Folding','Cleaning_Text','Normalisasi_Text','Formalisasi_Text','Remove_Text','Stemming_Text','Tokenize_Text','Processed_Text']])
 
-        label_column = 'Processed_Text'  # Mengganti label kolom dengan hasil preprocessing
+        label_column = 'Processed_Text'
         
-        # Lakukan proses labelling dari kolom 'Hasil_Preprocessing'
         lexicon_positive = pd.read_excel('kamus_positive.xlsx')
         lexicon_positive_dict = {}
         for index, row in lexicon_positive.iterrows():
@@ -494,11 +436,9 @@ elif st.session_state.page == 'Preprocessing':
             sentimen = 'Positive' if score > 0 else 'Negative' if score < 0 else 'Neutral'
             return score, sentimen
         
-        # Pastikan df sudah didefinisikan sebelumnya
-        if 'df' not in locals():  # Cek apakah df sudah ada
-            df = st.session_state.uploaded_data  # Inisialisasi df Anda di sini (misalnya, df = pd.read_csv('data.csv'))
+        if 'df' not in locals():
+            df = st.session_state.uploaded_data
 
-        # Lakukan analisis sentimen
         results = df['Processed_Text'].apply(sentiment_analysis_lexicon_indonesia)
         results = list(zip(*results))
         df['Polarity Score'] = results[0]
@@ -507,7 +447,6 @@ elif st.session_state.page == 'Preprocessing':
         st.subheader('Data Setelah Preprocessing dan Labeling')
         st.dataframe(df[['Processed_Text', 'Polarity Score', 'Labels']])
 
-        # Simpan dataframe hasil preprocessing dan labeling di session state
         st.session_state.preprocessed_data = df
 
         labeled_dataset_path = save_labeled_dataset(df, "labeled_dataset.csv")
@@ -523,7 +462,7 @@ elif st.session_state.page == 'Predict':
     if st.session_state.preprocessed_data is None:
         st.warning('Mohon lakukan preprocessing terlebih dahulu.')
     else:
-        df = st.session_state.preprocessed_data  # Mendapatkan DataFrame dari session state
+        df = st.session_state.preprocessed_data
 
         if 'Processed_Text' in df.columns and 'Labels' in df.columns:
             st.subheader('Data Setelah Preprocessing dan Labeling')
@@ -543,11 +482,9 @@ elif st.session_state.page == 'Predict':
             X_train = st.session_state.vectorizer.fit_transform(non_empty_documents)
             Y_train = df['Labels'].map({'Positive': 1, 'Negative': -1, 'Neutral': 0})
 
-            # Terapkan SMOTE untuk oversampling kelas minoritas
             smote = SMOTE(random_state=42)
             X_train_resampled, Y_train_resampled = smote.fit_resample(X_train, Y_train)
 
-            # Buat DataFrame dari matriks TF-IDF
             tfidf_df = pd.DataFrame(X_train_resampled.toarray(), columns=st.session_state.vectorizer.get_feature_names_out())
 
             st.subheader('Hasil TF-IDF')
@@ -557,7 +494,6 @@ elif st.session_state.page == 'Predict':
 
             X_train, X_test, Y_train, Y_test = train_test_split(X_train_resampled, Y_train_resampled, test_size=0.2, stratify=Y_train_resampled, random_state=42)
             
-            # Class weight adjustment
             class_weights = compute_class_weight('balanced', classes=np.unique(Y_train), y=Y_train)
             class_weight_dict = {i: class_weights[i] for i in range(len(class_weights))}
 
